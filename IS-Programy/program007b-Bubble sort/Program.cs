90d7985c48f814deb8fcd1a1935ea3a9bbb7429b
Rudolf Rydl <rydlous1@gmail.com>
2025-11-19 10:18:55 +0100

    #13 Bubble sort

---
+++ IS-Programy/program007b-Bubble sort/Program.cs
@@ -0,0 +1,115 @@
+﻿using System.Diagnostics;
+using System.Security.Authentication.ExtendedProtection;
+
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*******************************************");
+    Console.WriteLine("****************Bubble sort****************");
+    Console.WriteLine("*******************************************");
+    Console.WriteLine("***************Rudolf Rýdl*****************");
+    Console.WriteLine("*******************************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
+    int n;
+
+    while (!int.TryParse(Console.ReadLine(), out n))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte znovu počet čísel: ");
+    }
+
+    Console.Write("Zadejte dolní mez (celé číslo): ");
+    int lowerBound;
+
+    while (!int.TryParse(Console.ReadLine(), out lowerBound))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte znovu dolní mez: ");
+    }
+
+    Console.Write("Zadejte horní mez (celé číslo): ");
+    int upperBound;
+
+    while (!int.TryParse(Console.ReadLine(), out upperBound))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte znovu horní mez: ");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("================================================");
+    Console.WriteLine("Zadané hodnoty:");
+    Console.WriteLine("Počet čísel: {0}; Dolní mez: {1}; Horní mez: {2}", n, lowerBound, upperBound);
+    Console.WriteLine("================================================");
+
+    // Deklarace pole
+    int[] myRandNumbs = new int[n];
+
+    //Random myRandNumb = new Random(50); // generování stejných čísel při stejném vstupu - hodí se pro testování
+    Random myRandNumb = new Random();
+
+    Console.WriteLine();
+    Console.WriteLine("================================================");
+    Console.WriteLine("Pseudonáhodná čísla:");
+    for (int i = 0; i < n; i++)
+    {
+        myRandNumbs[i] = myRandNumb.Next(lowerBound, upperBound);
+        Console.Write("{0}; ", myRandNumbs[i]);
+    }
+
+
+    Stopwatch myStopwatch = new Stopwatch();
+
+    int compare = 0;
+    int change = 0;
+
+    // Algoritmus Bubble sort
+    myStopwatch.Start();
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            compare++;
+            if (myRandNumbs[j] < myRandNumbs[j + 1])
+            {
+                int tmp = myRandNumbs[j + 1];
+                myRandNumbs[j + 1] = myRandNumbs[j];
+                myRandNumbs[j] = tmp;
+                change++;
+            }
+        }
+    }
+    myStopwatch.Stop();
+
+
+
+    // Výpis seřazených čísel
+
+    Console.WriteLine();
+    Console.WriteLine("================================================");
+    Console.WriteLine();
+    Console.WriteLine("Seřazená čísla");
+    for (int i = 0; i < n - 1; i++)
+
+    Console.WriteLine();
+    Console.WriteLine($"Počet Porovnání: {compare}");
+    Console.WriteLine($"Počet Výměn: {change}");
+    Console.WriteLine("Čas potřebný na seřazení čísel: {0}", myStopwatch.Elapsed);
+
+    // Druhé největší číslo
+    int secondLargest = myRandNumbs[1];
+    Console.WriteLine($"\nDruhé největší číslo: {secondLargest}");
+
+     // Vykreslení obrazce podle druhého největšího čísla
+    Console.WriteLine("\nObrazec (trojúhelník):");
+
+    for (int i = 1; i <= secondLargest; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+    }
+
+}   
