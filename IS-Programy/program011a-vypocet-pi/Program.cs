da2c1b67a50be7588fbccb57b403967a76a6a753
Rudolf Rydl <rydlous1@gmail.com>
2025-12-03 09:45:18 +0100

    #17 výpočet pí

---
+++ IS-Programy/program011a-vypocet-pi/Program.cs
@@ -0,0 +1,45 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("****************************");
+    Console.WriteLine("***** Výpočet PI *****");
+    Console.WriteLine("****************************");
+    Console.WriteLine("******* Tomáš Žižka ********");
+    Console.WriteLine("****************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte přesnost (reálné číslo - čím menší hodnota, tím bude výpočet přesnější): ");
+    float presnost;
+    while(!float.TryParse(Console.ReadLine(), out presnost)) {
+        Console.Write("Nezadali jste reálné číslo, zadejte přesnost znovu: ");
+    }
+
+    float i = 1;
+    float znamenko = 1;
+    float piCtvrt = 1;
+
+    while((1/i)>=presnost) {
+        i = i + 2;
+        znamenko = -znamenko;
+        piCtvrt = piCtvrt + znamenko * (1/i);
+
+        if(znamenko==1) {
+            Console.WriteLine("Zlomek: +1/{0}; aktuální hodnota PI = {1}", i, 4 * piCtvrt);
+        }
+        else {
+            Console.WriteLine("Zlomek: -1/{0}; aktuální hodnota PI = {1}", i, 4 * piCtvrt);
+        }
+    }
+
+    Console.WriteLine("\n\n Hodnota čísla PI = {0}", 4 * piCtvrt);
+    //Console.WriteLine("\n\n Hodnota čísla PI = {0:f4}", 4 * piCtvrt);
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+
+
+}
+
+
